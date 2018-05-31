4f815042d1757fadc317a463e3c2c2573470a221
parkertewell <parkertewell@localhost>
2018-05-31 12:31:05 -0400

    yote

--- Monopoly/Front End/MainMenu.java
+++ Monopoly/Front End/MainMenu.java
@@ -7,8 +7,6 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.TimerTask;
 
 import javax.imageio.ImageIO;
 import javax.swing.ButtonGroup;
@@ -28,71 +26,45 @@ public class MainMenu extends JPanel implements ActionListener{
 	}
 	MainMenu() {
 		this.setLayout(null);		
-
-		//		for(int i = 0; i < panes.length; i++) {
-		//			panes[i] = new JPanel();
-		//			panes[i].setBackground(Color.WHITE);
-		//			this.add(panes[i]);
-		//		}
-		JLabel Title =new JLabel("TransAmerica");
-		Title.setFont(new Font("Arail", 1, 100));
-		Title.setSize(700, 75);
-		Title.setLocation(800-350, 50);
-		this.add(Title);
-		for(int i = 0; i <= 2; i++) {
-			playerPanels[i] = new PlayerPanel(i + 1);
-			playerPanels[i].setSize(350, 290);
-			playerPanels[i].setLocation(800+(450*(i-1))-175,250-100);
-			this.add(playerPanels[i]);
-		}
-
-		for(int i = 3; i <= 5; i++) {
-			playerPanels[i] = new PlayerPanel(i + 1);
-			playerPanels[i].setSize(350, 290);
-			playerPanels[i].setLocation(800+(450*(i-4))-175,550-100);
-			this.add(playerPanels[i]);
-		}
-		for(int i = 0; i < buttons.length; i++) {
-			buttons[i].addActionListener(this);
-			buttons[i].setActionCommand(buttons[i].getText());
-			buttons[i].setSize(100,50);
-			buttons[i].setLocation((int) (800+200*(i-.5))-50,760);
-			this.add(buttons[i]);
-		}
+		JLabel title = new JLabel("TransAmerica");
+		title.setFont(new Font("Arail", 1, 100));
+		title.setSize(700, 75);
+		title.setLocation(450, 50);
+		add(title);
+		
+//		for(int i = 0; i <= 2; i++) {
+//			playerPanels[i] = new PlayerPanel(i + 1);
+//			playerPanels[i].setSize(350, 290);
+//			playerPanels[i].setLocation(800+(450*(i-1))-175,250-100);
+//			this.add(playerPanels[i]);
+//		}
+//
+//		for(int i = 3; i <= 5; i++) {
+//			playerPanels[i] = new PlayerPanel(i + 1);
+//			playerPanels[i].setSize(350, 290);
+//			playerPanels[i].setLocation(800+(450*(i-4))-175,550-100);
+//			this.add(playerPanels[i]);
+//		}
+//		for(int i = 0; i < buttons.length; i++) {
+//			buttons[i].addActionListener(this);
+//			buttons[i].setActionCommand(buttons[i].getText());
+//			buttons[i].setSize(100,50);
+//			buttons[i].setLocation((int) (800+200*(i-.5))-50,760);
+//			this.add(buttons[i]);
+//		}
 //		try{
 //			backg= ImageIO.read(new File("Pix/TransAmerica Background.jpg"));
 //		}catch(Exception E){}
 
 	}
-	BufferedImage backg;
-	public void paint(Graphics g){
-		//g.drawImage(backg, 0, 0, 1600, 900, null);
-		for(int i=0;i<this.getComponentCount();i++){
-			g.translate(this.getComponent(i).getX(), this.getComponent(i).getY());
-			this.getComponent(i).paint(g);
-			g.translate(-this.getComponent(i).getX(), -this.getComponent(i).getY());
-		}
-
-	}
-
-//	public ArrayList<Player> generate(ArrayList<Color> playerColors, ArrayList<String> playerNames, ArrayList<String> playerType){
-//		ArrayList<Player> players = new ArrayList<Player>();
-//		boolean slowMode = false;
-//		for(int i = 0; i<playerType.size();i++){
-//			if(playerType.get(i).equals("Human")){
-//				players.add(new HumanPlayer(playerColors.get(i),playerNames.get(i)));
-//				slowMode = true;
-//			}else if(playerType.get(i).equals("Easy")){
-//				players.add(new EasyStrategy(playerColors.get(i),playerNames.get(i)));
-//			}else{
-//				int[] playerScores = new int[players.size()];
-//				for(int j = 0;j<playerScores.length;j++){
-//					playerScores[j] = 12;
-//				}
-//				players.add(new HardStrategy(playerColors.get(i),playerScores, playerNames.get(i)));
-//			}
+//	BufferedImage backg;
+//	public void paint(Graphics g){
+//		//g.drawImage(backg, 0, 0, 1600, 900, null);
+//		for(int i=0;i<this.getComponentCount();i++){
+//			g.translate(this.getComponent(i).getX(), this.getComponent(i).getY());
+//			this.getComponent(i).paint(g);
+//			g.translate(-this.getComponent(i).getX(), -this.getComponent(i).getY());
 //		}
-//		return players;
 //
 //	}
 	class PlayerPanel extends JPanel implements ActionListener{
---
+++ Monopoly/Front End/Monopoly.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+public class Monopoly{
+	static JFrame monopoly;
+	public Monopoly() {
+		System.out.println("test");
+		monopoly = new JFrame();
+		monopoly.setTitle("Monopoly");
+		monopoly.add(new MainMenu());
+		monopoly.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		monopoly.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		monopoly.setVisible(true);
+	}
+	public static void main(String[] args){
+		new Monopoly();
+	}
+}
